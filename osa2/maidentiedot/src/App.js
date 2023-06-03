b05bb214a81684c48edfb5ef93a70130093e4a2a
SaarinOsk <oskari.a.saarinen@gmail.com>
2023-06-03 11:42:38 +0300

    part of osa6 tasks

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,97 +1,16 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
-const Input = ({text, handleChange, value}) => {
-  return (
-    <div>
-      {text}<input value={value} onChange={handleChange} />
-    </div>
-  )
-}
-
-const Countries = ({countries, setFilter, weatherData}) => {
-  if (countries.length === 1) {
-    const c = countries[0]
-    console.log(weatherData)
-    return (
-      <div>
-        <h2>{c.name.common}</h2>
-        <p>capital {c.capital}</p>
-        <p>area {c.area}</p>
-        <h4>languages: </h4>
-        <ul>
-          {Object.values(c.languages).map(language => 
-            <li key={language}>{language}</li>
-          )}
-        </ul>
-        
-        <img src={c.flags.png} />
-
-        <h2>Weather in {c.capital}</h2>
-        <p>temperature {weatherData.main.temp} Celsius</p>
-        <img  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} />
-        <p>wind {weatherData.wind.speed}</p>
-      </div>
-    )
-  }
-
-  else {
-
-    return (
-      <div>
-        {countries.map(country => 
-          <div key={country.name.official}>
-            {country.name.common}
-            <button onClick={() => setFilter(country.name.common)}>show</button>
-          </div>
-        )}
-      </div>
-    )
-  }
-}
-
 const App = () => {
-  const [countries, setCountries] = useState([])
-  const [newFilter, setNewFilter] = useState('')
-  const [currentLatLong, setLatLong] = useState([0.0, 0.0])
-  const [weatherData, setWeatherData] = useState({})
-
-  const api_key = process.env.REACT_APP_API_KEY
-
-  useEffect(() => {
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => setCountries(response.data))  
-  }, [])
-  
-  useEffect(() => {
-    axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${currentLatLong[0]}&lon=${currentLatLong[1]}&appid=${api_key}`)
-      .then(response => setWeatherData(response.data))  
-  }, [currentLatLong])
-
-  const handleFilter = (event) => {
-    setNewFilter(event.target.value)
-  }
-
-  const countriesToShow = () => {
-    const realFilter = newFilter.toLowerCase()
-
-    const filtered = countries.filter(p => p.name.common.toLowerCase().includes(realFilter))
-    if(filtered.length < 10) {
-      return filtered
-    }
-    else {return []}
-  }
-
-  useEffect(() => {if(countriesToShow().length === 1) {setLatLong(countriesToShow()[0].capitalInfo.latlng)}})
-
   return (
-    <div>
-      <Input text={"find countries"} value={newFilter} handleChange={handleFilter} />
-      <Countries countries={countriesToShow()} setFilter={setNewFilter} weatherData={weatherData} />
-    </div>
+    <Sum a={1} b={2} />
   )
 }
-
+const Sum = (props) => {
+  const { a, b } = props
+  return(
+    <div>
+      {a} + {b} = {a + b}
+     </div>
+  )
+}
 export default App
